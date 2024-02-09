95ca5534c63c1f6d721c9dd5f0c62f527cea434c
danimisme <msubhanr53@gmail.com>
2024-02-09 22:18:05 +0700

    membuat fungsi makeCoffee

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-console.log("Menyalakan mesin kopi");
-console.log("Menggiling biji kopi");
-console.log("Memanaskan air");
-console.log("Mencampurkan air dan kopi");
-console.log("Menuangkan kopi ke dalam gelas");
-console.log("Menuangkan susu ke dalam gelas");
-console.log("Kopi Anda sudah siap!");
-
 const coffeeStock = require("./state");
 
-console.log(coffeeStock);
+const maceCoffee = (type, miligrams) => {
+  if (coffeeStock[type] >= miligrams) {
+    console.log(`kopi ${type} berhasil dibuat!`);
+  } else {
+    console.log(`Biji kopi ${type} tidak cukup!`);
+  }
+};
+
+maceCoffee("robusta", 80);
+maceCoffee("arabica", 150);
